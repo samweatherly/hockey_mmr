fcec8950223a5f8e56c23e7b9b0fddb1e775fa96
Sam Weatherly <weatherlysam@gmail.com>
2016-03-23 16:15:57 -0700

    basic team CRUD

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,5 @@
 class TeamsController < ApplicationController
+before_action :find_team, only: [:show, :edit, :update, :destroy]
 
   def new
     @team = Team.new
@@ -13,12 +14,39 @@ class TeamsController < ApplicationController
     end
   end
 
+  def show
+    # before_action :find_team
+  end
+
+  def index
+    @teams = Team.all
+  end
+
+  def edit
+    # before_action :find_team
+  end
+
+  def update
+    # before_action :find_team
+    @team.update team_params
+    redirect_to @team
+  end
+
+  def destroy
+    # before_action :find_team
+    @team.destroy
+    redirect_to teams_path
+  end
 
   private
 
   def team_params
     params.require(:team).permit( :name, :mmr, :k_value, :home_mmr, :away_mmr,
                                   :logo, :active)
+  end
+
+  def find_team
+    @team = Team.find params[:id]
   end
 
 end
