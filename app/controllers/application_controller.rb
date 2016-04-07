c05b10d73bc5836df5b6afd22bc8f7a73b08f31c
Sam Weatherly <weatherlysam@gmail.com>
2016-04-06 17:25:59 -0700

    css, applying classes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,23 @@ class ApplicationController < ActionController::Base
   # Adds below types to flash so they can be written the same as notice: "MESSAGE"
   add_flash_types :success, :error, :alert
 
+  def date_format(holder)
+    month = holder.month.to_s
+    day = holder.day.to_s
+    if month.length == 1
+      month = "0" + month
+    end
+    if day.length == 1
+      day = "0" + day
+    end
+    "#{day}/#{month}/#{holder.year}"
+  end
+  helper_method :date_format
+
+  def date_display(date)
+    month = Date::MONTHNAMES[date.month]
+    "#{month} #{date.day}"
+  end
+  helper_method :date_display
+
 end
--- app/models/future.rb
+++ app/models/future.rb
@@ -12,4 +12,30 @@ class Future < ActiveRecord::Base
     away_team.name
   end
 
+  def date_timeline
+    # DD/MM/YYYY
+    holder = self.date
+    month = holder.month.to_s
+    day = holder.day.to_s
+    if month.length == 1
+      month = "0" + month
+    end
+    if day.length == 1
+      day = "0" + day
+    end
+    "#{day}/#{month}/#{holder.year}"
+  end
+
+  def away_logo
+    away_team.logo.url(:thumb)
+  end
+  def home_logo
+    home_team.logo.url(:thumb)
+  end
+
+  # def date_display
+  # holder = self.date
+  # "#{holder.month} #{holder.day}"
+  # end
+
 end
