63fb8c45e95d8ee410b9b878aaf522fecfc8b1f5
Sam Weatherly <weatherlysam@gmail.com>
2016-04-12 16:10:08 -0700

    uncommented scheduled jobs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,8 +2,8 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-# DailyScrape.schedule!
-# UpcomingGames.schedule!
+DailyScrape.schedule!
+UpcomingGames.schedule!
 
 
 # Add additional assets to the asset load path
